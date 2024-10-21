baf9118b8add225ee865523fea479f3bcbe23ea9
marta-dot <dabrowskam2003@gmail.com>
2024-10-21 10:52:44 +0200

    Comment in program.java
    add coment java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,9 +1,14 @@
 public class Main {
+//komentarz
+...
+  int z = 4;
   int x = 5;
+hhh
   int y = 3;
 
   public static void main(String[] args) {
     Main myObj = new Main();
+     System.out.println(myObj.z);
     System.out.println(myObj.x);
     System.out.println(myObj.y);
   }
